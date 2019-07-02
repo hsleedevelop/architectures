baf347f31a0344629b129021af79e47135fb864c
HS Lee <hyungsuk.lee@iriver.com>
2019-07-02 16:25:56 +0900

    Initial Commit

---
+++ architectures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  architectures
+//
+//  Created by HS Lee on 02/07/2019.
+//  Copyright © 2019 HS Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
